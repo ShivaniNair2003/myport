180db639098ec279c7023f7cd055e78da0102f93
ShivaniNair2003 <111459651+ShivaniNair2003@users.noreply.github.com>
2023-05-08 22:42:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shivani Nair Lekshmi-Web Developer,Programmer,UI/UX Designer</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="sidebar">
+        Shivani
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>My Intro</li>
+                <li>Services</li>
+                <li>CV</li>
+                <li>Blog</li>
+            </ul>
+        </nav>
+    </div>
+    <div class="main">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem pariatur quas quam? Ullam sint quis, itaque eligendi optio sequi nihil, suscipit veniam dolorem recusandae eos iure consequatur impedit, delectus incidunt!
+    </div>
+</body>
+</html>
